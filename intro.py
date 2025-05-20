b368fdf188f774b3fa0720428aa7f9f28dba3185
AshleeHODOR <hodor1987@yahoo.com>
2025-05-19 21:13:51 -0700

    first commit

---
+++ intro.py
@@ -0,0 +1,27 @@
+print("Hello World!")
+
+#this is a comment
+#print("Here")
+
+#creating Variables
+#do not declare data type
+
+#creating VARIABLES
+name = "Ashleeeeeeeeeeeeeee"
+last_name = "hodor"
+#use underscores _ _ _ 
+print(name + " " +last_name) #Ashlee HODOR 
+found = False 
+age = 21
+middle_name = 'Rose' 
+
+print(name+" "+middle_name+" "+last_name) #Ashlee HODOR 
+#the type( function helps us to knwo the data type of the variable)
+print(type(name))
+print(type(age))
+print(type(found)) 
+
+#Caasting
+#Helps us to convert different data types
+#str()
+#int()
---
+++ story.py
@@ -0,0 +1,17 @@
+#variables
+name = "cat"
+age = 29
+middle_name = "iceland"
+last_name = "earth"
+
+print(type(name))
+print(type(age))
+print(type(last_name)) 
+
+print("I am" + name + "and i like to eat" + last_name + str(age) + "times, because it's good") 
+
+#the F Format
+#f""" """ 
+print(f""" 
+I am {name} and I like to eat {age} because it was so good {middle_name} 
+""")
